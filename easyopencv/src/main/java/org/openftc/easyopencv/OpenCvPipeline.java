dbb802e30007e84f34ad24489b06c3594394caf2
Windwoes <26417650+windwoes@users.noreply.github.com>
2023-02-07 11:26:39 -0500

    Improve leak detection warning

--- easyopencv/src/main/java/org/openftc/easyopencv/OpenCvPipeline.java
+++ easyopencv/src/main/java/org/openftc/easyopencv/OpenCvPipeline.java
@@ -21,8 +21,12 @@
 
 package org.openftc.easyopencv;
 
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
 import android.os.Debug;
 
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
 import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
@@ -50,6 +54,10 @@ public abstract class OpenCvPipeline
     private int gcRuns = 0;
     private String lastLeakMsg = "";
     private long lastLeakMsgUpdateTime;
+
+    private static final ActivityManager activityManager = (ActivityManager) AppUtil.getDefContext().getSystemService(Context.ACTIVITY_SERVICE);
+
+    private ActivityManager.MemoryInfo memoryInfo = new ActivityManager.MemoryInfo();
 
     public OpenCvPipeline()
     {
@@ -111,10 +119,15 @@ public abstract class OpenCvPipeline
 
         double absoluteDeltaAllocMB = absoluteDeltaAlloc / (1024.0*1024.0);
 
+        // if the native heap shrinks by more than 20MB that probably(?) means the GC ran
         if((absoluteDeltaAllocMB-previousAbsoluteDeltaAllocMB) < -20)
         {
-            System.out.println("GC probably ran");
-            gcLeakOffsetMb += previousAbsoluteDeltaAllocMB;
+            System.out.println("EasyOpenCV: native heap shrunk by > 20MB; GC probably ran");
+
+            // Assume that the GC reclaimed all memory it could, but we want to keep track
+            // of the running sum of how much it probably reclaimed, so we can try to guesstimate
+            // what the average leak rate is.
+            gcLeakOffsetMb += previousAbsoluteDeltaAllocMB; /* assume it reclaimed the entire delta, probably not realistic but it's the best we got */
             gcRuns++;
         }
 
@@ -122,11 +135,16 @@ public abstract class OpenCvPipeline
 
         double timeSinceStartedMonitoring = (System.currentTimeMillis() - firstMonitoredFrameTimestamp)/1000.0;
 
+        // Guesstimate the average leak rate using our best guess of the running sum of reclaimed memory
         double leakRate = (absoluteDeltaAllocMB+gcLeakOffsetMb) / timeSinceStartedMonitoring;
 
+        // We use a fairly generous threshold before showing a warning to avoid false positives.
         if((absoluteDeltaAllocMB+gcLeakOffsetMb) > MEMLEAK_THRESHOLD_MB)
         {
-            leakMsg = String.format("Pipeline likely leaking memory @ %dMB/sec; total of %dMB leaked; GC likely run %d times", (int)leakRate, (int)(absoluteDeltaAllocMB+gcLeakOffsetMb), gcRuns);
+            activityManager.getMemoryInfo(memoryInfo);
+            float availMemPercent = ((float) memoryInfo.availMem / (float) memoryInfo.totalMem)*100;
+
+            leakMsg = String.format("OpenCV pipeline leaking memory @ approx. %dMB/sec; %d%% RAM currently free. DO NOT create new Mats or re-assign Mat variables inside processFrame()!", (int)leakRate, (int)availMemPercent);
         }
         else
         {
